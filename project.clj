241db544a36a624b5ffd43431a170521c533a5f2
Aelfsyg <aelfsyg@gmail.com>
2017-09-15 15:24:46 +0700

    Fix project file for Clojars deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject git-sourcing "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "git-sourcing helps you keep local and remote git repositories synchronised."
+  :url "https://github.com/dcprevere/git-sourcing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
